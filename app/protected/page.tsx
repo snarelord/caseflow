abe88b9e1764b7f5c75996224ba6af761d47406d
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-04-10 19:47:45 +0100

    task fetching

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -3,7 +3,8 @@ import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 import styles from "./page.module.css";
-import Tasks from "../tasks/page";
+import GetTasks from "../tasks/GetTasks";
+import { AddTask } from "../tasks/actions";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -18,6 +19,9 @@ export default async function ProtectedPage() {
 
   // fetch tasks and filter by status
   const { data: tasks, error } = await supabase.from("tasks").select();
+
+  // console.log("tasks", tasks);
+
   if (error) {
     return <p>Error fetching tasks: {error.message}</p>;
   }
@@ -28,22 +32,33 @@ export default async function ProtectedPage() {
 
   return (
     <div className={styles.page}>
+      <form action={AddTask} className={styles.taskForm}>
+        <input name="title" placeholder="Title" required />
+        <textarea name="description" placeholder="Description" />
+        <select name="status" required>
+          <option value="To Do">To Do</option>
+          <option value="In Progress">In Progress</option>
+          <option value="Done">Done</option>
+        </select>
+        <input name="due_date" type="datetime-local" placeholder="Due date" required />
+        <button type="submit">Add Task</button>
+      </form>
       <div className={styles.section}>
         <h2 className={styles.heading}>To Do</h2>
         <div className={styles.taskSection}>
-          <Tasks tasks={toDoTasks} />
+          <GetTasks tasks={toDoTasks} />
         </div>
       </div>
       <div className={styles.section}>
         <h2 className={styles.heading}>In Progress</h2>
         <div className={styles.taskSection}>
-          <Tasks tasks={inProgressTasks} />
+          <GetTasks tasks={inProgressTasks} />
         </div>
       </div>
       <div className={styles.section}>
         <h2 className={styles.heading}>Completed</h2>
         <div className={styles.taskSection}>
-          <Tasks tasks={completedTasks} />
+          <GetTasks tasks={completedTasks} />
         </div>
       </div>
       <div className={styles.section}>
